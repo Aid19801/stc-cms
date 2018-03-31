9c24a6ddee6416c94c59a9ee0f0d61c40098a7b9
Adrian Thompson <adrian.thompson@sky.uk>
2018-03-31 13:24:41 +0100

    mocks and bootstrap css added

--- src/App.js
+++ src/App.js
@@ -3,16 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      isLoaded: false,
+      data: null
+    }
+  }
+
+  componentDidMount() {
+    this.getDB();
+  }
+
+  getDB() {
+    fetch('http://localhost:3004/data/')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          data: json,
+          isLoaded: true
+        })
+      })
+
+    console.log('FUBAR: ', this.state);
+  }
+
   render() {
+
+    const { isLoaded } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">STC CMS Editor</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        
+        { isLoaded ? <h2>loaded!</h2> : <h4>loading...</h4>}
+
+        <button className="btn btn-warning">click me</button>
       </div>
     );
   }
